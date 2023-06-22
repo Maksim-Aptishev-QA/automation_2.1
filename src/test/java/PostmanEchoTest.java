fd36233191c8676628c6f17462520449d59f0807
Maksim Aptishev GitHub <maximus.testqa777@gmail.com>
2023-06-22 23:00:24 +0300

    failed

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,2 +1,24 @@
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class PostmanEchoTest {
+
+    @Test
+    public void testData1() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"));
+
+    }
 }
+
+
